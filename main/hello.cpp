7b3ea6c2fdd1542c4e944a54eaa32d3a401a2355
Chockalingam <chockalingam424@gmail.com>
2024-03-09 18:40:04 +0530

    updatedd hello.cpp 3

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Webhook succesfully enabled";
+  cout << "Webhook succesfully enabled\n";
   return 0;
 }
